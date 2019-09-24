d623ca061f1bde8332a4778711036277c380a0cf
lamhung <lamhungypl@gmail.com>
2019-09-24 17:07:36 +0700

    Updates

---
+++ precache-manifest.3bccfaf7c624e85f7013c45821e61d18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7566e08592b78b9034d63328d6fec6",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "5b0cf17449189a923aaf",
+    "url": "/weatherapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2746f2a866b8d369c0e1",
+    "url": "/weatherapp/static/css/main.39f45790.chunk.css"
+  },
+  {
+    "revision": "5b0cf17449189a923aaf",
+    "url": "/weatherapp/static/js/2.7ae6dade.chunk.js"
+  },
+  {
+    "revision": "2746f2a866b8d369c0e1",
+    "url": "/weatherapp/static/js/main.752c4f21.chunk.js"
+  },
+  {
+    "revision": "d3b99bcde07c0df333db",
+    "url": "/weatherapp/static/js/runtime-main.c2cefd07.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weatherapp/static/media/bg.b6796c73.jpg"
+  }
+]);
